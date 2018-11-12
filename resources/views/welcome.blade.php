9459306bf536c85f668ac4256bcc95bc9dff3237
Kyle Klaus <kklaus@indemnity83.com>
2018-11-11 18:21:26 -0800

    dump potentially useful information to the homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,77 +20,107 @@
                 margin: 0;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
             .position-ref {
                 position: relative;
             }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
                 text-align: center;
+                margin: 0 25px;
             }
 
             .title {
                 font-size: 84px;
             }
 
-            .links > a {
+            .m-md {
+                margin-top: 30px;
+                margin-bottom: 30px;
+            }
+
+            .key {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 20px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
+                min-width: 400px;
+                text-align: right;
             }
 
-            .m-b-md {
-                margin-bottom: 30px;
+            .value {
+                text-align: left;
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 20px;
+                line-height: 30px;
+            }
+
+            .row {
+                display: flex;
+                padding: 10px 0;
+                margin-bottom: 20px;
+            }
+
+            .row:nth-child(even) {
+                background-color: #f8fafc;
+            }
+
+            code {
+                white-space: nowrap;
+                overflow-x: scroll;
+            }
+
+            pre {
+                box-sizing: border-box;
+                margin: 0;
+                font-size: 14px;
+                padding: 15px 20px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: #ccc;
+                background-color: #fcfcfc;
+                overflow: scroll;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+        <div class="position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="m-md">
+                    <div class="title">Proxy Check</div>
+                    <span>A sanity tool from <a href="https://github.com/indemnity83">Kyle Klaus</a></span>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+                <hr />
+
+                <div class="row">
+                    <div class="key">Is From Trusted Proxy</div>
+                    <div class="value"><code>{{ request()->isFromTrustedProxy() ? 'Yes' : 'No'}}</code></div>
+                </div>
+
+                <div class="row">
+                    <div class="key">Is Secure</div>
+                    <div class="value"><code>{{ request()->isSecure() ? 'Yes' : 'No'}}</code></div>
+                </div>
+
+                <div class="row">
+                    <div class="key">Client IPs</div>
+                    <div class="value"><code>{{ implode(', ', request()->getClientIps()) }}</code></div>
+                </div>
+
+                <div class="row">
+                    <div class="key">Request Headers</div>
+                    <div class="value">
+                        <code>
+                            @foreach(request()->header() as $header => $values)
+                                <strong>{{ title_case($header)  }}:</strong> {{ implode(', ', $values) }} <br />
+                            @endforeach
+                        </code>
+                    </div>
                 </div>
             </div>
         </div>
